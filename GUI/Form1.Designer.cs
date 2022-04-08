f51525c659b75ef21c1a85596c34001e92ed2d3c
Jose Luis Martinez Castañeda <jmartinez02@TSUBAKIMOTO.MX>
2022-04-08 15:26:23 -0500

    Se suben los archivos iniciales.

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCambio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(35, 12);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(221, 56);
+            this.txtNumero.TabIndex = 0;
+            this.txtNumero.Text = "4.5";
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(281, 16);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(139, 52);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "label1";
+            // 
+            // btnCambio
+            // 
+            this.btnCambio.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCambio.Location = new System.Drawing.Point(12, 88);
+            this.btnCambio.Name = "btnCambio";
+            this.btnCambio.Size = new System.Drawing.Size(666, 55);
+            this.btnCambio.TabIndex = 2;
+            this.btnCambio.Text = "VER";
+            this.btnCambio.UseVisualStyleBackColor = true;
+            this.btnCambio.Click += new System.EventHandler(this.btnCambio_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 155);
+            this.Controls.Add(this.btnCambio);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Form1";
+            this.Text = "Decimales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCambio;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCambio_Click(object sender, EventArgs e)
+        {
+            string contenido = txtNumero.Text;
+            string parte = contenido.Split('.')[1];
+            int cantidad = parte.Length;
+            string decimales = "";
+
+            if (cantidad == 4)
+            {
+                //if (parte[0].Equals('0'))
+                //{
+                //    decimales = "0" + parte[1] + parte[2] + parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0'))
+                //{
+                //    decimales = "00" + parte[2] + parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0') && parte[2].Equals('0'))
+                //{
+                //    decimales = "000"+parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0') && parte[2].Equals('0') && parte[3].Equals('0'))
+                //{
+                //    decimales = "0000";
+                //}
+
+            }
+
+            if (cantidad == 3)
+            {
+                //if (parte[0].Equals('0'))
+                //{
+                //    decimales = "0" + parte[1] + parte[2] + parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0'))
+                //{
+                //    decimales = "00" + parte[2] + parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0') && parte[2].Equals('0'))
+                //{
+                //    decimales = "000" + parte[3];
+                //}
+                //if (parte[0].Equals('0') && parte[1].Equals('0') && parte[2].Equals('0') && parte[3].Equals('0'))
+                //{
+                //    decimales = "0000";
+                //}
+            }
+            
+            if (cantidad == 2)
+            {
+                if (parte[0].Equals('0') && parte[1].Equals('0'))
+                {
+                    decimales = "0000";
+                }
+
+                if (parte[0].Equals('0') && parte[1] != '0')
+                {
+                    decimales = "000" + parte[1];
+                }
+
+                if (parte[0] != '0' && parte[1].Equals('0'))
+                {
+                    decimales = "00" + parte[0] + parte[1];
+                }
+                if (parte[0] != '0' && parte[1] != '0')
+                {
+                    decimales = "00" + parte[0] + parte[1];
+                }
+            }
+
+            if (cantidad == 1)
+            {
+                if (parte[0].Equals('0'))
+                {
+                    decimales = "0000";
+                }
+                else
+                {
+                    decimales = "000" + parte[0];
+                }
+            }
+
+
+            //string cadena = string.Format("{0:0.##00}", contenido);
+            lblResultado.Text = decimales;
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar == (int)Keys.Enter)
+            {
+                btnCambio.PerformClick();
+            }
+        }
+    }
+}
